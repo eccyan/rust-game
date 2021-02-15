eb88ffc3f7d98f0c2e3305d4f3e8055b3f22cb51
eccyan <g00.eccyan@gmail.com>
2021-02-15 20:15:36 +0900

    Init commit

---
+++ src/client.rs
@@ -0,0 +1,21 @@
+use game::greeter_client::GreeterClient;
+use game::HelloRequest;
+
+pub mod game {
+    tonic::include_proto!("game");
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Tonic".into(),
+    });
+
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
